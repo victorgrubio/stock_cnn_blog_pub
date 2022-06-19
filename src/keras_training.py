8da7a0c6275c95a6ed567379325d099e7dd81530
Victor <victorgrubiodl@gmail.com>
2022-06-19 10:42:58 +0200

    Resampled jupyter into script

--- src/keras_training.py
+++ src/keras_training.py
@@ -1,15 +1,23 @@
+import os
 from operator import itemgetter
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
+from sklearn.metrics import f1_score, cohen_kappa_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.utils import compute_class_weight
 
-from tensorflow.keras import backend as K
+from tensorflow.python.keras import backend as K
 import tensorflow as tf
+from tensorflow import optimizers
+from tensorflow.python.keras import Sequential, regularizers
+from tensorflow.python.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.python.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.utils.vis_utils import plot_model
 
 
 def get_sample_weights(y):
@@ -41,7 +49,6 @@ def reshape_as_image(x, img_width, img_height):
         x_temp[i] = np.reshape(x[i], (img_height, img_width))
 
     return x_temp
-
 
 
 def f1_weighted(y_true, y_pred):
@@ -115,6 +122,77 @@ def f1_metric(y_true, y_pred):
     return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
 
 
+def create_model_cnn(params):
+    model = Sequential()
+
+    print("Training with params {}".format(params))
+
+    conv2d_layer1 = Conv2D(params["conv2d_layers"]["conv2d_filters_1"],
+                           params["conv2d_layers"]["conv2d_kernel_size_1"],
+                           strides=params["conv2d_layers"]["conv2d_strides_1"],
+                           kernel_regularizer=regularizers.l2(params["conv2d_layers"]["kernel_regularizer_1"]),
+                           padding='same', activation="relu", use_bias=True,
+                           kernel_initializer='glorot_uniform',
+                           input_shape=(x_train[0].shape[0],
+                                        x_train[0].shape[1], x_train[0].shape[2]))
+    model.add(conv2d_layer1)
+    if params["conv2d_layers"]['conv2d_mp_1'] > 1:
+        model.add(MaxPool2D(pool_size=params["conv2d_layers"]['conv2d_mp_1']))
+
+    model.add(Dropout(params['conv2d_layers']['conv2d_do_1']))
+    if params["conv2d_layers"]['layers'] == 'two':
+        conv2d_layer2 = Conv2D(params["conv2d_layers"]["conv2d_filters_2"],
+                               params["conv2d_layers"]["conv2d_kernel_size_2"],
+                               strides=params["conv2d_layers"]["conv2d_strides_2"],
+                               kernel_regularizer=regularizers.l2(params["conv2d_layers"]["kernel_regularizer_2"]),
+                               padding='same', activation="relu", use_bias=True,
+                               kernel_initializer='glorot_uniform')
+        model.add(conv2d_layer2)
+
+        if params["conv2d_layers"]['conv2d_mp_2'] > 1:
+            model.add(MaxPool2D(pool_size=params["conv2d_layers"]['conv2d_mp_2']))
+
+        model.add(Dropout(params['conv2d_layers']['conv2d_do_2']))
+
+    model.add(Flatten())
+
+    model.add(Dense(params['dense_layers']["dense_nodes_1"], activation='relu'))
+    model.add(Dropout(params['dense_layers']['dense_do_1']))
+
+    if params['dense_layers']["layers"] == 'two':
+        model.add(Dense(params['dense_layers']["dense_nodes_2"], activation='relu',
+                        kernel_regularizer=params['dense_layers']["kernel_regularizer_1"]))
+        model.add(Dropout(params['dense_layers']['dense_do_2']))
+
+    model.add(Dense(3, activation='softmax'))
+
+    if params["optimizer"] == 'rmsprop':
+        optimizer = optimizers.RMSprop(lr=params["lr"])
+    elif params["optimizer"] == 'sgd':
+        optimizer = tf.optimizers.SGD(lr=params["lr"], decay=1e-6, momentum=0.9, nesterov=True)
+    elif params["optimizer"] == 'adam':
+        optimizer = optimizers.Adam(learning_rate=params["lr"], beta_1=0.9, beta_2=0.999, amsgrad=False)
+
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy', f1_metric])
+
+    return model
+
+def f1_custom(y_true, y_pred):
+    y_t = np.argmax(y_true, axis=1)
+    y_p = np.argmax(y_pred, axis=1)
+    f1_score(y_t, y_p, labels=None, average='weighted', sample_weight=None, zero_division='warn')
+
+
+def check_baseline(pred, y_test):
+    print("size of test set", len(y_test))
+    e = np.equal(pred, y_test)
+    print("TP class counts", np.unique(y_test[e], return_counts=True))
+    print("True class counts", np.unique(y_test, return_counts=True))
+    print("Pred class counts", np.unique(pred, return_counts=True))
+    holds = np.unique(y_test, return_counts=True)[1][2]  # number 'hold' predictions
+    print("baseline acc:", (holds/len(y_test)*100))
+
+
 if __name__ == "__main__":
     np.random.seed(2)
     company_code = 'WMT'
@@ -125,6 +203,7 @@ if __name__ == "__main__":
     if 'dividend_amount' in df.columns:
         df.drop(columns=['dividend_amount', 'split_coefficient'], inplace=True)
     print(df.head())
+
 
     list_features = list(df.loc[:, 'open':'eom_26'].columns)
     print('Total number of features', len(list_features))
@@ -248,3 +327,72 @@ if __name__ == "__main__":
         plt.subplots_adjust(wspace=0.2, hspace=0.2)
         plt.imshow(img)
     plt.show()
+
+    params = {'batch_size': 80,
+              'conv2d_layers': {'conv2d_do_1': 0.2, 'conv2d_filters_1': 32, 'conv2d_kernel_size_1': 3, 'conv2d_mp_1': 0,
+                                'conv2d_strides_1': 1, 'kernel_regularizer_1': 0.0, 'conv2d_do_2': 0.3,
+                                'conv2d_filters_2': 64, 'conv2d_kernel_size_2': 3, 'conv2d_mp_2': 2,
+                                'conv2d_strides_2': 1,
+                                'kernel_regularizer_2': 0.0, 'layers': 'two'},
+              'dense_layers': {'dense_do_1': 0.3, 'dense_nodes_1': 128, 'kernel_regularizer_1': 0.0, 'layers': 'one'},
+              'epochs': 3000, 'lr': 0.001, 'optimizer': 'adam'}
+
+    model = create_model_cnn(params)
+    plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=False)
+
+    best_model_path = os.path.join('.', 'best_model_keras')
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,
+                       patience=100, min_delta=0.0001)
+    # csv_logger = CSVLogger(os.path.join(OUTPUT_PATH, 'log_training_batch.log'), append=True)
+    rlp = ReduceLROnPlateau(monitor='val_loss', factor=0.02, patience=20, verbose=1, mode='min',
+                            min_delta=0.001, cooldown=1, min_lr=0.0001)
+    mcp = ModelCheckpoint(best_model_path, monitor='val_f1_metric', verbose=1,
+                          save_best_only=True, save_weights_only=False, mode='max', period=1)  # val_f1_metric
+
+    history = model.fit(x_train, y_train, epochs=params['epochs'], verbose=1,
+                        batch_size=64, shuffle=True,
+                        # validation_split=0.3,
+                        validation_data=(x_cv, y_cv),
+                        callbacks=[mcp, rlp, es]
+                        , sample_weight=sample_weights)
+
+    plt.figure()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.plot(history.history['f1_metric'])
+    plt.plot(history.history['val_f1_metric'])
+
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['train_loss', 'val_loss', 'f1', 'val_f1'], loc='upper left')
+    plt.show()
+
+    model = load_model(best_model_path)
+    test_res = model.evaluate(x_test, y_test, verbose=0)
+    print("keras evaluate=", test_res)
+    pred = model.predict(x_test)
+    pred_classes = np.argmax(pred, axis=1)
+    y_test_classes = np.argmax(y_test, axis=1)
+    check_baseline(pred_classes, y_test_classes)
+    conf_mat = confusion_matrix(y_test_classes, pred_classes)
+    print(conf_mat)
+    labels = [0, 1, 2]
+
+    f1_weighted = f1_score(y_test_classes, pred_classes, labels=None,
+                           average='weighted', sample_weight=None)
+    print("F1 score (weighted)", f1_weighted)
+    print("F1 score (macro)", f1_score(y_test_classes, pred_classes, labels=None,
+                                       average='macro', sample_weight=None))
+    print("F1 score (micro)", f1_score(y_test_classes, pred_classes, labels=None,
+                                       average='micro',
+                                       sample_weight=None))  # weighted and micro preferred in case of imbalance
+
+    # https://scikit-learn.org/stable/modules/model_evaluation.html#cohen-s-kappa --> supports multiclass; ref: https://stats.stackexchange.com/questions/82162/cohens-kappa-in-plain-english
+    print("cohen's Kappa", cohen_kappa_score(y_test_classes, pred_classes))
+
+    recall = []
+    for i, row in enumerate(conf_mat):
+        recall.append(np.round(row[i] / np.sum(row), 2))
+        print("Recall of class {} = {}".format(i, recall[i]))
+    print("Recall avg", sum(recall) / len(recall))
